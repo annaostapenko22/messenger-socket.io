110dcb59673b8237150a2cde861c404aa3b39426
Anna Ostapenko <kvintabudinvest@gmail.com>
2020-03-21 00:17:12 +0200

    finished project

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ app.use(express.static("public"));
 
 const io = socket(server);
 
-io.on("connection", (socket)=> {
-    console.log("Made socket connection", socket.id)
-})
+io.on("connection", socket => {
+  console.log("Made socket connection", socket.id);
+
+  socket.on("chat", data => {
+    io.sockets.emit("chat", data);
+  });
+
+  socket.on("typing", data => {
+    socket.broadcast.emit("typing", data);
+  });
+});
--- public/chat.js
+++ public/chat.js
@@ -1,1 +1,34 @@
-const socket  = io.connect("http://localhost:4000")
+const socket = io.connect("http://localhost:4000");
+
+const refs = {
+  message: document.getElementById("message"),
+  handle: document.getElementById("handle"),
+  button: document.getElementById("send"),
+  output: document.getElementById("output"),
+  feedback: document.getElementById("feedback")
+};
+
+const handleClickSend = () => {
+  socket.emit("chat", {
+    message: refs.message.value,
+    handle: refs.handle.value
+  });
+  refs.message.value = "";
+};
+
+const broadcastTypingMessage = () => {
+  socket.emit("typing", refs.handle.value);
+};
+
+refs.button.addEventListener("click", handleClickSend);
+refs.message.addEventListener("keypress", broadcastTypingMessage);
+
+socket.on("chat", data => {
+  refs.feedback.innerHTML = "";
+  refs.output.innerHTML += `<p><strong> ${data.handle} : </strong> ${data.message}</p>`;
+  
+});
+
+socket.on("typing", data => {
+  refs.feedback.innerHTML = `<p><em> ${data} is typing a message...</em></p>`;
+});
